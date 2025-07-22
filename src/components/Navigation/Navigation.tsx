4d14ffc8378084628eecf9d867f3c5bd04e1b5a7
Daniel Filipe <72364158+Daniel10013@users.noreply.github.com>
2025-07-21 23:42:36 -0300

    Correcao nav bar

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,21 @@
 import { NavLink } from 'react-router-dom';
-import type { NavLinkProps } from 'react-router-dom';
+import type { NavLinkRenderProps } from 'react-router-dom';
 import './Navigation.css';
 
 function Navigation() {
-    const activeClass = ({ isActive }: NavLinkProps) => isActive ? "active-link" : "";
+  const activeClass = ({ isActive }: NavLinkRenderProps) =>
+    isActive ? 'active-link' : '';
 
-    return (
-        <nav>
-            <NavLink to="/home" className={activeClass}>
-                Home
-            </NavLink>
-            <NavLink to="/gerar-artigo" className={activeClass}>
-                Gerar Artigo
-            </NavLink>
-        </nav>
-    );
-}   
+  return (
+    <nav>
+      <NavLink to="/home" className={activeClass}>
+        Home
+      </NavLink>
+      <NavLink to="/gerar-artigo" className={activeClass}>
+        Gerar Artigo
+      </NavLink>
+    </nav>
+  );
+}
 
 export default Navigation;
